9a7162f67345b5a03fc099e967b57fbae04badb5
lesegomolako <lsgmlk1@gmail.com>
2020-08-29 06:57:45 +0200

    products commit

--- ExperTech Api/Controllers/ProductsController.cs
+++ ExperTech Api/Controllers/ProductsController.cs
@@ -5,6 +5,10 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Dynamic;
+using Microsoft.Ajax.Utilities;
 
 namespace ExperTech_Api.Controllers
 {
@@ -51,9 +55,10 @@ namespace ExperTech_Api.Controllers
         }
 
         [Route("api/Products/UpdateProduct")]
-        [HttpPut]
+        [HttpPost]
         public dynamic UpdateProduct([FromBody] Product Modell)
         {
+
             Product findProduct = db.Products.Where(zz => zz.ProductID == Modell.ProductID).FirstOrDefault();
             findProduct.Name = Modell.Name;
             findProduct.Price = Modell.Price;
@@ -69,7 +74,7 @@ namespace ExperTech_Api.Controllers
         {
             Product findProduct = db.Products.Where(zz => zz.ProductID == ProductID).FirstOrDefault();
 
-            foreach(ProductPhoto photo in findProduct.ProductPhotoes)
+            foreach (ProductPhoto photo in findProduct.ProductPhotoes)
             {
                 db.ProductPhotoes.Remove(photo);
                 db.SaveChanges();
@@ -79,7 +84,73 @@ namespace ExperTech_Api.Controllers
             db.SaveChanges();
             return "success";
 
+
         }
 
+        [Route("api/Products/GetProducts")]
+        [HttpGet]
+        public dynamic GetProducts()
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            List<Product> findProduct = db.Products.Include(zz => zz.ProductCategory)
+                .Include(zz => zz.ProductPhotoes).Include(zz => zz.Supplier).ToList();
+
+            return GetProds(findProduct);
+        }
+
+        private dynamic GetProds(List<Product> Modell)
+        {
+            List<dynamic> myList = new List<dynamic>();
+            foreach (Product items in Modell)
+            {
+                dynamic newObject = new ExpandoObject();
+                newObject.ProductID = items.ProductID;
+                newObject.Name = items.Name;
+                newObject.QuantityOnHand = items.QuantityOnHand;
+                newObject.Description = items.Description;
+                newObject.Price = items.Price;
+                newObject.Category = items.ProductCategory.Category;
+                newObject.Supplier = items.Supplier.Name;
+                newObject.CategoryID = items.CategoryID;
+                newObject.SupplierID = items.SupplierID;
+                newObject.Photos = getPhotos(items);
+
+                myList.Add(newObject);
+            }
+            return myList;
+        }
+
+        private dynamic getPhotos(Product Modell)
+        {
+            List<dynamic> mylist = new List<dynamic>();
+            foreach (ProductPhoto items in Modell.ProductPhotoes)
+            {
+                dynamic newObject = new ExpandoObject();
+                newObject.PhotoID = items.PhotoID;
+                newObject.Photo = items.Photo;
+
+                mylist.Add(newObject);
+            }
+
+            return mylist;
+        }
+
+        [Route("api/Products/getSuppliers")]
+        [HttpGet]
+        public List<Supplier> getSuppliers()
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            List<Supplier> getSupplier = db.Suppliers.ToList();
+            return getSupplier;
+        }
+
+        [Route("api/Products/getCategories")]
+        [HttpGet]
+        public List<ProductCategory> getCategories()
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            List<ProductCategory> getCategory = db.ProductCategories.ToList();
+            return getCategory;
+        }
     }
 }
